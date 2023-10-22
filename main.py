01e813f50525aa6cc8f4808306f85803fcfc5d75
misaki. <misakiigmd@gmail.com>
2023-10-22 23:35:41 +0200

    Added image download

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tweepy
+import requests
+import shutil
 
 # Import our Twitter credentials from credentials.json
 import json
@@ -12,5 +14,18 @@ auth = tweepy.OAuth1UserHandler(creds["API_KEY"], creds["API_SECRET"], creds["AC
 # Create API object
 api = tweepy.API(auth)
 
-# Create a tweet
-client.create_tweet(text="Hello, world!")
+# Get NASA Astronomy Picture of the Day from the API
+r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={creds['NASA_API_KEY']}")
+data = r.json()
+
+img_url = data["hdurl"]
+file_name = data["date"] + ".jpg"
+
+res = requests.get(img_url, stream=True)
+if res.status_code == 200:
+    with open(file_name, "wb") as file:
+        shutil.copyfileobj(res.raw, file)
+    print("Image downloaded successfully.")
+    shutil.move(file_name, "images/")
+else:
+    print("Image couldn't be retreived.")
